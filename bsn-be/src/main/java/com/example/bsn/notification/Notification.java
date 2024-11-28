2b1ddbb0be410e74f183e69c025ddca6409c1a5d
Lahiru <lahiruviet@gmail.com>
2024-11-28 13:10:42 +0700

    initial development

---
+++ bsn-be/src/main/java/com/example/bsn/notification/Notification.java
@@ -0,0 +1,21 @@
+package com.example.bsn.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Notification {
+
+    private NotificationStatus status;
+    private String message;
+    private String bookTitle;
+}
---
+++ bsn-be/src/main/java/com/example/bsn/notification/NotificationStatus.java
@@ -0,0 +1,8 @@
+package com.example.bsn.notification;
+
+public enum NotificationStatus {
+
+    BORROWED,
+    RETURNED,
+    RETURN_APPROVED
+}
